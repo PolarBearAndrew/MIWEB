cc7a4110adddd5b6e2e05a3868db617289fda346
AndrewChen <PolarBearAndrew@users.noreply.github.com>
2016-05-08 14:54:28 +0800

    update

--- v1/login.php
+++ v1/login.php
@@ -36,13 +36,15 @@
 	$password = $_POST['password'];
 
 	if ($memberType == 1) {
-		$sqlmember = "select memberid,checkauth,compid from member where disabled!=1 and compid=0 and account='$account' and password='$password' and checkauth=1";
+		$sqlmember = "select memberid,checkauth,compid from member where disabled!=1 and compid=0 and account='$account' and password='$password'";
 	}
 	elseif ($memberType == 2) {
-		$sqlmember = "select memberid,checkauth,compid from member where disabled!=1 and compid='$compid' and account='$account' and password='$password' and checkauth=1";
+		$sqlmember = "select memberid,checkauth,compid from member where disabled!=1 and compid='$compid' and account='$account' and password='$password'";
 	}
 
 	$stmtmember = sqlsrv_query($conn, $sqlmember);
+
+	echo sqlsrv_fetch($stmtmember);
 
 	if (sqlsrv_fetch($stmtmember)) {
 		return resData(false, 'Validate fail');
