4229e797b85fb524bde95f675cd6df86536f7402
AndrewChen <PolarBearAndrew@users.noreply.github.com>
2016-05-08 13:35:39 +0800

    mainQuery build query success

--- v1/mainQuery.php
+++ v1/mainQuery.php
@@ -3,16 +3,44 @@
 
 	$keys = array_keys($_POST);
 
-	$Query = 'SELECT * FROM ' . $_POST['table'];
+	$Query = 'SELECT @fields FROM ' . $_POST['table'];
 
 	// filter
 	$tmp = Array();
 	for ($i = 0; $i < count($keys); $i++) {
-		if($keys[$i] != 'table') {
+		if(
+			$keys[$i] != 'table' &&
+			$keys[$i] != 'where'
+		) {
 			array_push($tmp, $keys[$i]);
 		}
 	}
+	$keys = $tmp;
 
-	// echo print_r($tmp);
+	// replace fields
+	$fields = '';
+	for ($i = 0; $i < count($keys); $i++) {
+		if($i != 0) {
+			$fields = $fields . ', ';
+		}
+		$fields = $fields . $keys[$i];
+	}
+	$Query = str_replace("@fields", $fields, $Query);
+
+	// replace where
+	if($_POST['where']) {
+		$where = ' WHERE ';
+		$whereFields = array_keys($_POST['where']);
+		for ($i = 0; $i < count($whereFields); $i++) {
+			if($i != 0) {
+				$where = $where . ' AND ';
+			}
+			$key = $whereFields[$i];
+			$where = $where . $key . ' = ' . $_POST['where'][$key];
+		}
+		$Query = $Query . $where;
+	}
+
+	echo $Query;
 
 ?>
