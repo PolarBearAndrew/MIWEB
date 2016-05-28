0c4fe69d3697e543094e59a19ac149320aafce92
AndrewChen <PolarBearAndrew@users.noreply.github.com>
2016-05-28 17:11:06 +0800

    add main update

---
+++ v1/mainUpdate.php
@@ -0,0 +1,72 @@
+
+<?php
+
+	require_once ('config.php');
+	// require_once ('validateLogin.php');
+
+	$keys = array_keys($_POST);
+
+	$Query = 'UPDATE ' . $_POST['table'] . ' SET @fields WHERE ';
+
+	// filter
+	$tmp = Array();
+	for ($i = 0; $i < count($keys); $i++) {
+		if(
+			$keys[$i] != 'table' &&
+			$keys[$i] != 'where'
+		) {
+			array_push($tmp, $keys[$i]);
+		}
+	}
+	$keys = $tmp;
+
+	// replace fields
+	$fields = '';
+	for ($i = 0; $i < count($keys); $i++) {
+		if(
+			$i != 0 ||
+			$i != count($keys) - 1
+		) {
+			$fields = $fields . ', ';
+		}
+		$fields = $fields . $keys[$i]. ' = ' . $_POST[$keys[$i]];
+	}
+	$Query = str_replace("@fields", $fields, $Query);
+
+	// replace where
+	if($_POST['where']) {
+		$where = ' WHERE ';
+		$whereFields = array_keys($_POST['where']);
+		for ($i = 0; $i < count($whereFields); $i++) {
+			if($i != 0) {
+				$where = $where . ' AND ';
+			}
+			$key = $whereFields[$i];
+			$where = $where . $key . ' = ' . $_POST['where'][$key];
+		}
+		$Query = $Query . $where;
+	}
+
+	echo $Query;
+
+	// $params = array();
+	// $options = array(
+	// 	"Scrollable" => SQLSRV_CURSOR_KEYSET
+	// );
+	// $result = sqlsrv_query($conn, $Query, $params, $options);
+	//
+	// if ($result === false) {
+	// 	die(print_r(sqlsrv_errors() , true));
+	// }
+	// else {
+	// 	$data = Array();
+	// 	while ($row = sqlsrv_fetch_array($result)) {
+	// 		array_push($data, $row);
+	// 	}
+	// 	$data = array(
+	// 		'data' => $data
+	// 	);
+	//
+	// 	echo json_encode($data);
+	}
+?>
